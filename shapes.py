425e9cb547e36160012769451f86da23ded57aaa
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-08 07:30:00 -0500

    added customizable bounds to shapes.py as well

--- shapes.py
+++ shapes.py
@@ -10,10 +10,15 @@ print("a + bi")
 c = float(input("a: "))
 d = float(input("b: "))
 depth = int(input("Depth: "))
+x1, x2, y1, y2 = list(map(float, input("Enter bounds here in the format \"x1 x2 y1 y2\":").split(" ")))
 
 # function that computes the squaring and addition of two imaginary points a + bi and c + di
 xFunc = lambda a, b: (a+c)**2-(b+d)**2
 yFunc = lambda a, b: 2*(a+c)*(b+d)
+
+# gets a random number within the bounds
+def randomInBounds(min, max):
+    return (random.random()) * (max - min) + min
 
 # calculates distance from center of the point
 def test(a, b):
@@ -24,9 +29,9 @@ def test(a, b):
     return math.sqrt(a**2 + b**2)
 
 while True:
-    # sets bounds for the point generation
-    e = (random.random() - 0.5) * 4
-    f = (random.random() - 0.5) * 4
+    # sets bounds for the point generation - usually +-2
+    e, f = randomInBounds(x1, x2), randomInBounds(y1, y2)
+
     try:
         # gets value to be analyzed
         val = test(e, f)
